0de9b3769dad18c2e237c84b9d9f7e72638e791a
Gabriela Papin <gabrielapapin20@gmail.com>
2022-08-10 22:43:31 -0300

    added react-hook-form controller, created alert when login is ok and added all text in react-i18next

--- src/components/signIn/signin.tsx
+++ src/components/signIn/signin.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useAppDispatch } from 'redux/hooks';
 import { email } from 'redux/reducers/signin';
+import { useTranslation } from 'react-i18next';
 
 type FormData = {
   email: string;
@@ -12,57 +13,52 @@ type FormData = {
 }
 
 const schema = Yup.object({
-  email: Yup.string().email().required(),
+  email: Yup.string().required(),
   password: Yup.string().min(8).required(),
 }).required();
 
 const signIn: FC = () => {
-  const { register, handleSubmit, reset, formState: { errors } } = useForm<FormData>({
+  const navigate = useNavigate();
+  const { handleSubmit, control, reset, formState: { errors } } = useForm<FormData>({
     resolver: yupResolver(schema),
-    defaultValues: {email: '', password: ''}
   });
   const dispatch = useAppDispatch();
+  const { t } = useTranslation();
 
-  const onSubmit = handleSubmit((values) => {
+  const onSubmit: SubmitHandler<FormData> = values => {
     dispatch(email(values.email));
-    
-    if (errors.email) {
-        alert('Invalid Email or Password');
-    } else {
-        alert('You have sucessfully logged in');
-    }
-
+      // alert('Invalid Email or Password');
+    alert('You have sucessfully logged in');
     reset({ email: '', password: '' });
-  });
+    navigate('/home');
+  };
 
   return (
     <>
-      <h1>Get Job</h1>
-      <h2>THE BEST WAY TO FIND YOUR JOB!</h2>
-      <form onSubmit={onSubmit}>
-        <label>
-          Username or Email
-          <input
-            id="email"
-            type="email"
-            {...register("email")}
+      <h1>{`${t('SignIn.title')}`}</h1>
+      <h2>{`${t('SignIn.upperText')}`}</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>{`${t('SignIn.email')}`}</label>
+          <Controller
+            render={({ field }) => <input type="text" {...field} />}
+            name="email"
+            control={control}
+            defaultValue="" 
           />
-        </label>
-        <p>{errors.email?.message}</p>
-        <label>
-          Password
-          <input
-            id="password"
-            type="password" 
-            {...register("password")}
+          {errors.email?.message}
+        <label>{`${t('SignIn.password')}`}</label>
+          <Controller
+            render={({ field }) => <input type="password" {...field} />}
+            name="password"
+            control={control}
+            defaultValue=""
           />
-          <Link to="">Forgot your password?</Link>
-        </label>
-        <p>{errors.password?.message}</p>
-        <button type="submit">Sign In</button>
+          <p>{errors.password?.message}</p>
+          <Link to="">{`${t('SignIn.forgotPassword')}`}</Link>
+        <button type="submit">{`${t('SignIn.buttonSignin')}`}</button>
       </form>
-      <p>Don`t have an account yet?</p>
-      <Link to="/signup">Register Now</Link>
+      <p>{`${t('SignIn.text')}`}</p>
+      <Link to="/signup">{`${t('SignIn.registerLink')}`}</Link>
     </>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sampleReducer from './reducers/sampleSlice';
+import signInReducer from './reducers/signin';
 
 export const store = configureStore({
   reducer: {
     sample: sampleReducer,
+    signIn: signInReducer
   },
 });
 
