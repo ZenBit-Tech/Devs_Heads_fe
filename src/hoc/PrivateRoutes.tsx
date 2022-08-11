8406dfafa8bbcaf258cbc14508660836693fc2a3
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-08-11 16:05:24 +0300

    fix react script

--- src/hoc/PrivateRoutes.tsx
+++ src/hoc/PrivateRoutes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoutes = ({ token }: { token: string | null }) => {
+	console.log(token);
 	return token ? <Outlet /> : <Navigate to="/login" />;
 };
 export default PrivateRoutes;
