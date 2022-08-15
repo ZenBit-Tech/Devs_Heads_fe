769d95587fb57d150953cf54de80ba87b96c91d2
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-08-15 12:30:58 +0300

    fix prettier tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  ignorePatterns: ['src/stories/components/StateOfChargeLinearChart/data*.ts', '.eslintrc.js', 'webpack.config.js'],
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  ignorePatterns: ["src/stories/components/StateOfChargeLinearChart/data*.ts", ".eslintrc.js", "webpack.config.js"],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
   rules: {
     "indent": ["error", "tab"],
-    "prettier/prettier": [2, { "useTabs": true }],
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    "@typescript-eslint/ban-ts-comment": "off",
-  },
+    "prettier/prettier": [2, { "useTabs": true, "tabWidth": 2 }],
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/ban-ts-comment": "off"
+  }
 };
