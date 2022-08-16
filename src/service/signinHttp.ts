e8201fa7ad2ea24ded20b8d5c9d4adb12738b1a1
Gabriela Papin <gabrielapapin20@gmail.com>
2022-08-16 14:59:34 -0300

    added the base url in .env file

--- src/service/signinHttp.ts
+++ src/service/signinHttp.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const authApi: any = createApi({
   reducerPath: 'auth',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
   endpoints: (build) => ({
     signIn: build.mutation({
       query: (body) => ({
