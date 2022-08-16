40f58077a4e46a5191de1cf5b5182c7a02475cf0
Vadim Vladimirov <kurtonchik@gmail.com>
2022-08-16 22:08:55 +0300

    done task

---
+++ src/components/WelcomePage/WelcomePageLayout.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const WelcomePageLayoutWrapper = styled.div`
+  text-align: center;
+`;
+
+export const TitleStyled = styled.div`
+  font-size: 40px;
+`;
+
+export const ImageStyled = styled.img`
+  width: 20px;
+  height: 20px;
+`;
---
+++ src/components/WelcomePage/WelcomePageLayout.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { WelcomePageLayoutWrapper, TitleStyled, ImageStyled } from './WelcomePageLayout.styles';
+import ArrowDowm from '../../assets/arrowDown.jpg';
+
+const WelcomePageLayout: FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <WelcomePageLayoutWrapper>
+      <TitleStyled>{`${t('WelcomePage.title')}`}</TitleStyled>
+      <p>{`${t('WelcomePage.description')}`}</p>
+      <p>{`${t('WelcomePage.pointerToLink')}`}</p>
+      <ImageStyled src={ArrowDowm} alt="arrowDown" />
+      <Link to="/settings">{`${t('WelcomePage.linkDescription')}`}</Link>
+    </WelcomePageLayoutWrapper>
+  );
+};
+
+export default WelcomePageLayout;
