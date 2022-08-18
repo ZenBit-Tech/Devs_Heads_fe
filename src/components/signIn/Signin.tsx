c3fb0c3b9a4229fb059e91c42a0c7b77ec09f76f
Gabriela Papin <gabrielapapin20@gmail.com>
2022-08-18 16:45:53 -0300

    merge with develop branch

--- src/components/signIn/Signin.tsx
+++ src/components/signIn/Signin.tsx
@@ -1,67 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Div, Div2, H1, H2, Form, Input, Button, ControlStyle, LinkStyle, P, ErrorP } from './Signin.styles';
+import {
+	Div,
+	Div2,
+	H1,
+	H2,
+	Form,
+	Input,
+	Button,
+	ControlStyle,
+	LinkStyle,
+	P,
+	ErrorP,
+} from './Signin.styles';
 import { useTranslation } from 'react-i18next';
 import { useSignInMutation } from 'service/signinHttp';
 
-type FormData = {
-  email: string;
-  password: string;
-}
+export type FormData = {
+	email: string;
+	password: string;
+};
 
 const schema = Yup.object({
-  email: Yup.string().required(),
-  password: Yup.string().min(8).required(),
+	email: Yup.string().required(),
+	password: Yup.string().min(8).required(),
 }).required();
 
 const signIn = () => {
-  const [signIn] = useSignInMutation();
-  const navigate = useNavigate();
-  const { handleSubmit, control, reset, formState: { errors } } = useForm<FormData>({
-    resolver: yupResolver(schema),
-  });
+	const [signIn] = useSignInMutation();
+	const [error, setError] = useState(false);
+	const [sucess, setSucess] = useState(false);
+	const navigate = useNavigate();
+	const {
+		handleSubmit,
+		control,
+		reset,
+		formState: { errors },
+	} = useForm<FormData>({
+		resolver: yupResolver(schema),
+	});
 
-  const { t } = useTranslation();
+	const { t } = useTranslation();
 
-  const onSubmit: SubmitHandler<FormData> = async (values: object) => {
-    await signIn(values);
-    alert('You have sucessfully logged in');
-    reset({ email: '', password: '' });
-    navigate('/home');
-  };
+	const onSubmit: SubmitHandler<FormData> = async values => {
+		await signIn(values)
+			.unwrap()
+			.then(() => {
+				setSucess(true);
+				console.log(sucess);
+				reset({ email: '', password: '' });
+				alert('You have sucessfully logged in');
+				navigate('/home');
+			})
+			.catch(() => {
+				setError(true);
+				console.log(error);
+				reset({ email: '', password: '' });
+				alert('Invalid email or username');
+			});
+	};
 
-  return (
-    <Div>
-      <H1>{`${t('SignIn.title')}`}</H1>
-      <H2>{`${t('SignIn.upperText')}`}</H2>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <ControlStyle>{`${t('SignIn.email')}`}</ControlStyle>
-          <Controller
-            render={({ field }) => <Input type="text" {...field} />}
-            name="email"
-            control={control}
-            defaultValue="" 
-          />
-        <ControlStyle>{`${t('SignIn.password')}`}</ControlStyle>
-          <Controller
-            render={({ field }) => <Input type="password" {...field} />}
-            name="password"
-            control={control}
-            defaultValue=""
-          />
-          <LinkStyle><Link to="/forgot-passowrd">{`${t('SignIn.forgotPassword')}`}</Link></LinkStyle>
-          <ErrorP>{errors.password?.message}</ErrorP>
-        <Button type="submit">{`${t('SignIn.buttonSignin')}`}</Button>
-      </Form>
-      <Div2>
-        <P>{`${t('SignIn.text')}`}</P>
-        <Link to="/signup">{`${t('SignIn.registerLink')}`}</Link>
-      </Div2>
-    </Div>
-  );
-}
+	return (
+		<Div>
+			<H1>{`${t('SignIn.title')}`}</H1>
+			<H2>{`${t('SignIn.upperText')}`}</H2>
+			<Form onSubmit={handleSubmit(onSubmit)}>
+				<ControlStyle>{`${t('SignIn.email')}`}</ControlStyle>
+				<Controller
+					render={({ field }) => <Input type="text" {...field} />}
+					name="email"
+					control={control}
+					defaultValue=""
+				/>
+				<ControlStyle>{`${t('SignIn.password')}`}</ControlStyle>
+				<Controller
+					render={({ field }) => <Input type="password" {...field} />}
+					name="password"
+					control={control}
+					defaultValue=""
+				/>
+				<LinkStyle>
+					<Link to="/forgot-passowrd">{`${t('SignIn.forgotPassword')}`}</Link>
+				</LinkStyle>
+				<ErrorP>{errors.password?.message}</ErrorP>
+				<Button type="submit">{`${t('SignIn.buttonSignin')}`}</Button>
+			</Form>
+			<Div2>
+				<P>{`${t('SignIn.text')}`}</P>
+				<Link to="/signup">{`${t('SignIn.registerLink')}`}</Link>
+			</Div2>
+		</Div>
+	);
+};
 
 export default signIn;
--- src/service/signinHttp.ts
+++ src/service/signinHttp.ts
@@ -1,21 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { FormData } from 'components/signIn/Signin';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const authApi: any = createApi({
-  reducerPath: 'auth',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
-  endpoints: (build) => ({
-    signIn: build.mutation({
-      query: (body) => ({
-        url: 'auth/sign-in',
-        method: 'post',
-        body,
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      }),
-    }),
-  }),
+export const authApi = createApi({
+	reducerPath: 'auth',
+	baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
+	endpoints: build => ({
+		signIn: build.mutation<{ email: string; password: string }, FormData>({
+			query: body => ({
+				url: 'auth/sign-in',
+				method: 'post',
+				body,
+				headers: {
+					'Content-type': 'application/json; charset=UTF-8',
+				},
+			}),
+		}),
+	}),
 });
 
 export const { useSignInMutation } = authApi;
