c93803212bd8df5face875dd405a409e8c3a8fa9
Gabriela Papin <gabrielapapin20@gmail.com>
2022-08-24 14:39:22 -0300

    connect forgot-password

--- src/service/httpService.ts
+++ src/service/httpService.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IContactInfoForm } from '../pages/settings-page/components/ContactInfo/ContactInfo';
+import { FormEmail } from 'components/forgotPassword/forgotPassword';
 
 type FormData = {
 	email: string;
@@ -33,9 +34,19 @@ export const authApi = createApi({
 				},
 			}),
 		}),
+		forgotPassword: build.mutation<{ email: string }, FormEmail>({
+			query: body => ({
+				url: 'auth/forgot-password',
+				method: 'post',
+				body,
+				headers: {
+					'Content-type': 'application/json; charset=UTF-8',
+				},
+			}),
+		}),
 	}),
 });
-export const { useSignUpMutation, useSignInMutation } = authApi;
+export const { useSignUpMutation, useSignInMutation, useForgotPasswordMutation } = authApi;
 
 /*TODO when backend is ready*/
 export const profileApi = createApi({
