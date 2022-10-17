714b5346eb1790ad96d12e5e09c3bb7a8d0e996c
Gabriela Papin <gabrielapapin20@gmail.com>
2022-10-17 14:07:08 -0300

    change url in .env to constant in googleAuth file

--- src/components/GoogleAuth/GoogleAuth.tsx
+++ src/components/GoogleAuth/GoogleAuth.tsx
@@ -3,10 +3,12 @@ import googleIcon from 'assets/googleIcon.png';
 import { Image, Typography, Container } from 'components/GoogleAuth/GoogleAuthStyle';
 import { useTranslation } from 'react-i18next';
 
+const REACT_APP_GOOGLE_REDIRECT = 'http://localhost:3000/auth/redirect';
+
 function GoogleAuth() {
 	const { t } = useTranslation();
 	const googleAuth = () => {
-		window.open(`${process.env.REACT_APP_GOOGLE_REDIRECT}`, '_self');
+		window.open(`${REACT_APP_GOOGLE_REDIRECT}`, '_self');
 	};
 	return (
 		<Container onClick={googleAuth}>
