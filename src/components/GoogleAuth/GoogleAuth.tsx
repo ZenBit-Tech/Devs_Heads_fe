ba187fb5d998035a516c2d2a44b40ccd4a9cccac
Gabriela Papin <gabrielapapin20@gmail.com>
2022-10-17 16:00:33 -0300

    fix .env urls

--- src/components/GoogleAuth/GoogleAuth.tsx
+++ src/components/GoogleAuth/GoogleAuth.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import googleIcon from 'assets/googleIcon.png';
 import { Image, Typography, Container } from 'components/GoogleAuth/GoogleAuthStyle';
 import { useTranslation } from 'react-i18next';
-import { REACT_APP_GOOGLE_REDIRECT } from 'constants/links';
 
 function GoogleAuth() {
 	const { t } = useTranslation();
 	const googleAuth = () => {
-		window.open(`${REACT_APP_GOOGLE_REDIRECT}`, '_self');
+		window.open(`${process.env.REACT_APP_GOOGLE_REDIRECT}`, '_self');
 	};
 	return (
 		<Container onClick={googleAuth}>
